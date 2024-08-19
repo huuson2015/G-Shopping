f3fd23bf3a088bc3111771c2241c0de857c692e5
Sonnguyen2015 <sonnhgcd201844@fpt.edu.vn>
2024-08-19 21:04:12 +0700

    Add: CRUD for Product

--- g-shopping-be/index.js
+++ g-shopping-be/index.js
@@ -6,6 +6,8 @@ import cookieParser from "cookie-parser";
 import connectDB from "./config/db.js";
 import userRoutes from "./routes/userRoutes.js";
 import categoryRoutes from "./routes/categoryRoute.js";
+import productRoutes from "./routes/productRoute.js";
+import uploadRoutes from "./routes/uploadRoute.js";
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -20,6 +22,8 @@ app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
 app.use("/api/category", categoryRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/upload", uploadRoutes);
 
 app.listen(port, () => {
 	console.log(`Server running on port: ${port}`);
---
+++ g-shopping-be/routes/productRoute.js
@@ -0,0 +1,36 @@
+import express from "express";
+import ExpressFormidable from "express-formidable";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+import checkId from "../middlewares/checkId.js";
+import {
+	addProduct,
+	updateProductDetails,
+	getProducts,
+	getAllProducts,
+	getProductById,
+	removeProduct,
+	addProductReview,
+	getTopProducts,
+	getNewProducts,
+} from "../controllers/productController.js";
+
+const router = express.Router();
+
+router
+	.route("/")
+	.get(getProducts)
+	.post(authenticate, authorizeAdmin, ExpressFormidable(), addProduct);
+
+router.route("/allproducts").get(getAllProducts);
+router.route("/:id/reviews").post(authenticate, checkId, addProductReview);
+
+router.route("/top").get(getTopProducts);
+router.route("/new").get(getNewProducts);
+
+router
+	.route("/:id")
+	.get(getProductById)
+	.put(authenticate, authorizeAdmin, ExpressFormidable(), updateProductDetails)
+	.delete(authenticate, authorizeAdmin, removeProduct);
+
+export default router;
