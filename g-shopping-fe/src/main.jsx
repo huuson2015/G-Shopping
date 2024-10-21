f2882638255465371e12f2b553af833e6a85a6cc
Sonnguyen2015 <sonnhgcd201844@fpt.edu.vn>
2024-10-21 23:31:07 +0700

    Add: PaypalProvider

--- g-shopping-fe/src/main.jsx
+++ g-shopping-fe/src/main.jsx
@@ -3,12 +3,15 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { PayPalScriptProvider } from "@paypal/react-paypal-js";
 import store from "@redux/store.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<Provider store={store}>
-		<BrowserRouter>
-			<App />
-		</BrowserRouter>
+		<PayPalScriptProvider>
+			<BrowserRouter>
+				<App />
+			</BrowserRouter>
+		</PayPalScriptProvider>
 	</Provider>
 );
