cdc640859d7052a2350519b71753dc95a510fcbc
Sonnguyen2015 <sonnhgcd201844@fpt.edu.vn>
2024-10-21 23:32:25 +0700

    Fix: missing import from Order

---
+++ g-shopping-fe/src/components/ProgressStep.jsx
@@ -0,0 +1,62 @@
+import { PropTypes } from "prop-types";
+import { HiMiniCheckCircle } from "react-icons/hi2";
+
+const ProgressStep = ({ step1, step2, step3 }) => {
+	return (
+		<div className="flex justify-center items-center space-x-4">
+			<div
+				className={`flex gap-2 justify-center items-center ${
+					step1 ? "text-green-500" : "text-gray-300"
+				}`}
+			>
+				<span>Login</span>
+				<HiMiniCheckCircle className={`${step1 ? "" : ""}`} size={30} />
+			</div>
+
+			{step2 && (
+				<>
+					{step1 && <div className="h-0.5 w-[10rem] bg-green-500"></div>}
+					<div
+						className={`flex gap-2 justify-center items-center ${
+							step1 ? "text-green-500" : "text-gray-300"
+						}`}
+					>
+						<span>Shipping</span>
+						<HiMiniCheckCircle size={30} />
+					</div>
+				</>
+			)}
+
+			<>
+				{step1 && step2 && step3 ? (
+					<div className="h-0.5 w-[10rem] bg-green-500"></div>
+				) : (
+					""
+				)}
+
+				<div
+					className={`flex gap-2 justify-center items-center ${
+						step3 ? "text-green-500" : "text-gray-300"
+					}`}
+				>
+					<span className={` ${!step3 ? "ml-[10rem]" : ""}`}>Summary</span>
+					{step1 && step2 && step3 ? (
+						<div className="mt-2 text-lg text-center">
+							<HiMiniCheckCircle size={30} />
+						</div>
+					) : (
+						""
+					)}
+				</div>
+			</>
+		</div>
+	);
+};
+
+ProgressStep.propTypes = {
+	step1: PropTypes.bool,
+	step2: PropTypes.bool,
+	step3: PropTypes.bool,
+};
+
+export default ProgressStep;
